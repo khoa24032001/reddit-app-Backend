9521d0c0476e298e2f36bd33db628adafbdb1699
Anh Khoa <anhkhoa240320012gmail.com>
2023-08-25 15:44:34 +0700

    ver1.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.post("v1/update", (res,req) => {
+app.post("/v1/update", (req,res) => {
     setTimeout(()=>{
         res.status(200).json(req.body);
     },[2000])
